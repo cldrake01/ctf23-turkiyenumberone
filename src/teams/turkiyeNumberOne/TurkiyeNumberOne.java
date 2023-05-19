a54c99e87e099e737e8ce96b02e982c6b9afdd89
cldrake01 <collinlindendrake@gmail.com>
2023-05-19 15:09:28 -0600

    //

--- src/teams/turkiyeNumberOne/TurkiyeNumberOne.java
+++ src/teams/turkiyeNumberOne/TurkiyeNumberOne.java
@@ -8,8 +8,8 @@ import info.gridworld.grid.Location;
 
 public class TurkiyeNumberOne extends Team {
 
-    public TurkiyeNumberOne(Color color) {
-        super(color);
+    public TurkiyeNumberOne() {
+        super(Color.RED);
 
         super.addPlayer(new CPlayer(new Location(5 + (int)(Math.random()*3 - 1), 49)));
         super.addPlayer(new QPlayer(new Location(10 + (int)(Math.random()*3 - 1), 30)));
