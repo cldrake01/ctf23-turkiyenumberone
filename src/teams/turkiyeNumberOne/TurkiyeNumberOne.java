faa8f0f3125a209f5db5c5d1c38136142c691ca0
Quentin Bona <quentin.t.bona@gmail.com>
2023-05-24 19:48:39 -0600

    ocd + bug fixes

--- src/teams/turkiyeNumberOne/TurkiyeNumberOne.java
+++ src/teams/turkiyeNumberOne/TurkiyeNumberOne.java
@@ -2,6 +2,8 @@ package teams.turkiyeNumberOne;
 import java.awt.Color;
 import ctf.Team;
 import info.gridworld.grid.Location;
+// Quentin made QPlayer with their associated methods, icons, and formatting
+// Collin made CPlayer with their associated methods, strategy and structure
 public class TurkiyeNumberOne extends Team {
     public TurkiyeNumberOne() {
         super(Color.RED);
