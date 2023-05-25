01bdf5051d03a5e50aec922bdee2208e5aedf633
Quentin Bona <quentin.t.bona@gmail.com>
2023-05-24 19:29:49 -0600

    Changes by collindrake

--- src/teams/turkiyeNumberOne/CPlayer.java
+++ src/teams/turkiyeNumberOne/CPlayer.java
@@ -1,6 +1,5 @@
 package teams.turkiyeNumberOne;
 import ctf.Player;
-import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 public class CPlayer extends BasePlayer {
     public CPlayer(Location startLocation) {
@@ -10,10 +9,7 @@ public class CPlayer extends BasePlayer {
     public Location getMoveLocation() {
         Location home = new Location(getLocation().getRow(), getMyTeam().getFlag().getLocation().getCol());
         if (this.hasFlag())
-            return evade() != null ? evade() : (getGrid().get(getLocation().getAdjacentLocation(getLocation().getDirectionToward(home))) instanceof Rock
-                    ? getGrid().getEmptyAdjacentLocations(getLocation().getAdjacentLocation(getLocation().getDirectionToward(home))).get((int) (Math.random() * getGrid().getEmptyAdjacentLocations(getLocation().getAdjacentLocation(getLocation().getDirectionToward(home))).size()))
-                    : home
-            );
+            return evade() != null ? evade() : home;
         else if (searchSurroundings() != null)
             return searchSurroundings();
         else if (getGrid().get(getLocation().getAdjacentLocation(getLocation().getDirectionToward(getOtherTeam().getFlag().getLocation()))) instanceof Player)
